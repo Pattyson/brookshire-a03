a1a7c77fa57806b93f61f101917888f59c48fd3f
Pattyson <84475829+Pattyson@users.noreply.github.com>
2021-10-03 19:06:53 -0400

    commit

--- exercise30/src/main/java/Solution30.java
+++ exercise30/src/main/java/Solution30.java
@@ -1,2 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Patrick Brookshire
+ */
+
 public class Solution30 {
+
+    public static void main(String[] args)
+    {
+        for(int i = 1 ; i <= 12; i++)
+        {
+            for(int j = 1; j <= 12; j++)
+            {
+                System.out.print(i*j + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
