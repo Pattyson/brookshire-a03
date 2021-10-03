740b4c361670bb2b29ee0fad18a35bf136122270
Pattyson <84475829+Pattyson@users.noreply.github.com>
2021-10-03 18:58:55 -0400

    commit

--- exercise28/src/main/java/Solution28.java
+++ exercise28/src/main/java/Solution28.java
@@ -1,2 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Patrick Brookshire
+ */
+
+import java.util.*;
+import java.lang.*;
+
 public class Solution28 {
+
+    public static void main (String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int count = 1;
+        int sum = 0;
+        int element;
+        while(count <6){
+            System.out.print("Enter the value: ");
+            element =sc.nextInt();
+            System.out.println(element);
+            sum +=element;
+            count++;
+        }
+        System.out.println("The total is " + sum);
+    }
 }
